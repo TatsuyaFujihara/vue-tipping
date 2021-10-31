253c7cc40540d25536dcb3b1860e862e5de5b5d6
TatsuyaFujihara <pjpktwagtdjt@gmail.com>
2021-10-31 17:05:42 +0900

    vue4-6

--- src/views/home.vue
+++ src/views/home.vue
@@ -24,21 +24,38 @@
           <td>{{otherUser.name}}</td>
           <td>
             <button class="wallet" @click="userWallet(index)">walletを見る</button>
-            <button class="send">送る</button>
+            <button class="send" @click="sendMoneyModal(index)">送る</button>
           </td>
         </tr>
+
           <transition name="fade" appear>
             <div class="modal-overlay" v-if="infoModal"></div>
           </transition>
           <transition name="slide" appear>
             <div class="modal" v-if="infoModal">
-              <p>{{name}}さんの残高</p>
-              <p>{{money}}</p>
+              <p>{{otherUserName}}さんの残高</p>
+              <p>{{therUserMoney}}</p>
               <button id="close" class="button" @click="infoModal = false">
                 閉じる
               </button>
             </div>
           </transition>
+
+          <transition name="fade" appear>
+            <div class="modal-overlay" v-if="sendModal"  @click="sendModal = false"></div>
+          </transition>
+          <transition appear>
+            <div class="modal" v-if="sendModal">
+              <div v-for="(user,index) in users" :key="index">
+                <p>あなたの残高：{{user.money}}</p>
+                <input type="number" v-model="tradeMoney">
+                <button id="close" class="button" @click="sendMoney">
+                  送信
+                </button>
+              </div>
+            </div>
+          </transition>
+
       </tbody>
     </table>
     
@@ -52,8 +69,13 @@ export default {
   data() {
     return {
         infoModal: false, 
-        name: "",
-        money: ""
+        sendModal: false, 
+
+        otherUserName: "",
+        otherUserMoney: "",
+
+        userId: "",
+        tradeMoney: "",
       }
   },
   created: function() {
@@ -73,9 +95,24 @@ export default {
     },
     userWallet: function(index){
       const userInfo = this.$store.getters.otherUser[index];
-      this.name = userInfo.name;
-      this.money = userInfo.money;
+      this.otherUserName = userInfo.name;
+      this.therUserMoney = userInfo.money;
       this.infoModal = true;
+    },
+    sendMoneyModal: function(index){
+      this.userId = index;
+      this.sendModal = true;
+    },
+    sendMoney: function(){
+      const userInfo = this.$store.getters.otherUser[this.userId];
+      const receiveUserAdd = userInfo.mail;
+      const userMoney = userInfo.money;
+            
+      this.$store.dispatch("sendMoney", {
+        receiveUser: receiveUserAdd,
+        userMoney: userMoney,
+        tradeMoney: this.tradeMoney,
+      });
     }
   }
 }
@@ -123,7 +160,8 @@ input {
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
-  align-items: center
+  align-items: center;
+  min-height: 100vh;
 }
 .modal {
  position: fixed;
@@ -145,7 +183,7 @@ input {
  padding: 25px;
 }
 .modal-overlay {
- position: absolute;
+ position: fixed;
  top: 0;
  left: 0;
  right: 0;
